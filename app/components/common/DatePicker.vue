2997e3abecc022b01f46ebb35ab6d087dc2e7d72
t-kurihara <takuya.kurihara00@gmail.com>
2019-01-02 17:53:56 +0900

    [add] Reseve page components.

---
+++ app/components/common/DatePicker.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-menu
+    :close-on-content-click="false"
+    v-model="menu"
+    :nudge-right="40"
+    lazy
+    transition="scale-transition"
+    offset-y
+    full-width
+    min-width="290px"
+  >
+    <v-text-field
+      slot="activator"
+      v-model="date"
+      label="Date"
+      prepend-icon="event"
+      readonly
+    />
+    <v-date-picker
+      v-model="date"
+      @input="menu = false"/>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  name: 'DatePicker',
+  data: () => ({
+    menu: false
+  })
+}
+</script>
